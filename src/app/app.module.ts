75281f554a18a40b2490dee4a0a50256830a6d5d
Fidores <45700014+Fidores@users.noreply.github.com>
2021-07-27 23:53:22 +0200

    Setup locale for Poland

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,9 @@
-import { NgModule } from '@angular/core';
+import {
+	DEFAULT_CURRENCY_CODE,
+	LOCALE_ID,
+	NgModule,
+	Provider,
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,19 +11,31 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatModule } from './mat.module';
 import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
+import { registerLocaleData } from '@angular/common';
+import localePL from '@angular/common/locales/pl';
+
+const PolishLocaleProvider: Provider = {
+	provide: LOCALE_ID,
+	useValue: 'pl-PL',
+};
+const PolishCurrencyCodeProvider: Provider = {
+	provide: DEFAULT_CURRENCY_CODE,
+	useValue: 'PLN',
+};
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainNavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, MainNavbarComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatModule,
+	],
+	providers: [PolishLocaleProvider, PolishCurrencyCodeProvider],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+	constructor() {
+		registerLocaleData(localePL, 'pl-PL');
+	}
+}
