5e88d985504c5b9286a982adcf9f93e5b92bbf52
Fidores <45700014+Fidores@users.noreply.github.com>
2021-08-04 22:09:21 +0200

    Added firestore configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+	production: true,
+	firestore: {
+		apiKey: 'AIzaSyBnAo2tRm1EWhapd1CPK9bC7lQmjYZOyB0',
+		authDomain: 'budget-monitor-c013c.firebaseapp.com',
+		projectId: 'budget-monitor-c013c',
+		storageBucket: 'budget-monitor-c013c.appspot.com',
+		messagingSenderId: '996068851259',
+		appId: '1:996068851259:web:39e7684497a5c9bb19261e',
+	},
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+	production: false,
+	firestore: {
+		apiKey: 'AIzaSyBnAo2tRm1EWhapd1CPK9bC7lQmjYZOyB0',
+		authDomain: 'budget-monitor-c013c.firebaseapp.com',
+		projectId: 'budget-monitor-c013c',
+		storageBucket: 'budget-monitor-c013c.appspot.com',
+		messagingSenderId: '996068851259',
+		appId: '1:996068851259:web:39e7684497a5c9bb19261e',
+	},
 };
 
 /*
