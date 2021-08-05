f5e369045aa00efd7971eaffea2276a64f76114a
Fidores <45700014+Fidores@users.noreply.github.com>
2021-08-05 21:45:56 +0200

    Implemented authorization logic

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
 	templateUrl: './login.component.html',
@@ -6,7 +8,32 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent implements OnInit {
-	constructor() {}
+	constructor(
+		private readonly _auth: AuthService,
+		private readonly _router: Router
+	) {}
 
 	ngOnInit(): void {}
+
+	async loginWithGoogle() {
+		try {
+			await this._auth.loginWithGoogle();
+			this._router.navigateByUrl('/');
+		} catch (error) {
+			this._handleError(error);
+		}
+	}
+
+	async loginAnonymously() {
+		try {
+			await this._auth.loginAnonymously();
+			this._router.navigateByUrl('/');
+		} catch (error) {
+			this._handleError(error);
+		}
+	}
+
+	private _handleError(error: any) {
+		console.log(error);
+	}
 }
