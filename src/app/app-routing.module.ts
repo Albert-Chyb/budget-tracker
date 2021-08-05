e96cac8be04307aaa450a7522e6b67187cc0e442
Fidores <45700014+Fidores@users.noreply.github.com>
2021-08-05 23:58:30 +0200

    Added auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import {
+	AngularFireAuthGuard,
+	redirectLoggedInTo,
+} from '@angular/fire/auth-guard';
+
+const redirectLoggedInToHome = () => redirectLoggedInTo(['/']);
 
 const routes: Routes = [
 	{
 		path: 'login',
 		component: LoginComponent,
+		canActivate: [AngularFireAuthGuard],
+		data: {
+			authGuardPipe: redirectLoggedInToHome,
+		},
 	},
 ];
 
