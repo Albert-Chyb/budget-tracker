15282b6d4cd9fa137c22a70b98359fcaf5388009
Fidores <45700014+Fidores@users.noreply.github.com>
2021-08-26 22:25:39 +0200

    Made wallet component to be dumb

--- src/app/components/wallet/wallet.component.ts
+++ src/app/components/wallet/wallet.component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { take } from 'rxjs/operators';
-import { INewWallet, IWallet } from 'src/app/common/interfaces/wallet';
-import { PromptService } from 'src/app/services/prompt/prompt.service';
-import { WalletsService } from 'src/app/services/wallets/wallets.service';
-
-import { NewWalletDialogComponent } from '../new-wallet-dialog/new-wallet-dialog.component';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	Input,
+	Output,
+	EventEmitter,
+} from '@angular/core';
+import { IWallet } from 'src/app/common/interfaces/wallet';
 
 @Component({
 	selector: 'wallet',
@@ -14,39 +14,10 @@ import { NewWalletDialogComponent } from '../new-wallet-dialog/new-wallet-dialog
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WalletComponent {
-	constructor(
-		private readonly _wallets: WalletsService,
-		private readonly _dialog: MatDialog,
-		private readonly _prompt: PromptService
-	) {}
+	constructor() {}
 
 	@Input('wallet') wallet: IWallet;
 	@Input('show-actions') showActions: boolean = false;
-
-	deleteWallet() {
-		this._wallets.delete(this.wallet);
-	}
-
-	updateName() {
-		const name$ = this._prompt.open({
-			title: 'Zmiana nazwy portfela',
-			label: 'Nowa nazwa',
-			value: this.wallet.name,
-		});
-
-		name$.pipe(take(1)).subscribe(name => {
-			if (name && name !== this.wallet.name)
-				this._wallets.updateName(this.wallet, name);
-		});
-	}
-
-	async createWallet() {
-		const data: INewWallet = await this._dialog
-			.open(NewWalletDialogComponent)
-			.afterClosed()
-			.pipe(take(1))
-			.toPromise();
-
-		if (data) this._wallets.create(data);
-	}
+	@Output('onWalletEdit') onWalletEdit = new EventEmitter<IWallet>();
+	@Output('onWalletDelete') onWalletDelete = new EventEmitter<IWallet>();
 }
