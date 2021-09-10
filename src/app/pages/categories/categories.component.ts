dda4257cc648f82abde035628d78e2949eaff0e4
Fidores <45700014+Fidores@users.noreply.github.com>
2021-09-10 22:35:15 +0200

    Added logic to create categories in firestore

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
-import { ICategory } from 'src/app/common/interfaces/category';
+import { filter, first } from 'rxjs/operators';
+import { ICategory, INewCategory } from 'src/app/common/interfaces/category';
+import { NewCategoryDialogComponent } from 'src/app/components/new-category-dialog/new-category-dialog.component';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
 import { LoadingService } from 'src/app/services/loading/loading.service';
 
@@ -12,14 +15,56 @@ import { LoadingService } from 'src/app/services/loading/loading.service';
 export class CategoriesComponent {
 	constructor(
 		private readonly _categories: CategoriesService,
-		private readonly _loading: LoadingService
+		private readonly _loading: LoadingService,
+		private readonly _dialog: MatDialog
 	) {}
 
 	categories$: Observable<ICategory[]> = this._loading.add(
 		this._categories.readAll()
 	);
 
+	addCategory() {
+		this._openDialog().subscribe(category => {
+			// TODO: Remove this line and implement file upload service.
+			category.icon = 'https://image.flaticon.com/icons/png/512/706/706164.png';
+
+			this._loading.add(this._categories.create(category));
+		});
+	}
+
+	editCategory(category: ICategory) {
+		this._openDialog(category).subscribe(newCategory => {
+			if (<any>newCategory.icon instanceof File) {
+				// TODO: Remove this line and implement file upload service.
+				newCategory.icon =
+					'https://image.flaticon.com/icons/png/512/174/174872.png';
+			}
+
+			this._loading.add(this._categories.update(category.id, newCategory));
+		});
+	}
+
+	deleteCategory(category: ICategory) {
+		this._loading.add(this._categories.delete(category.id));
+	}
+
 	trackById(index: number, category: ICategory) {
 		return category.id;
+	}
+
+	private _openDialog(data?: ICategory) {
+		return this._dialog
+			.open<NewCategoryDialogComponent, ICategory, INewCategory>(
+				NewCategoryDialogComponent,
+				{
+					width: '300px',
+					data,
+				}
+			)
+			.afterClosed()
+			.pipe(
+				first(),
+				filter(data => !!data)
+			);
 	}
 }
