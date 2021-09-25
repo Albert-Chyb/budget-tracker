b41bf5234638d7aec23a3257e907746c197be4f1
Fidores <45700014+Fidores@users.noreply.github.com>
2021-09-25 20:30:10 +0200

    Applied Angular Forms to the transaction form

--- src/app/pages/transaction/transaction.component.ts
+++ src/app/pages/transaction/transaction.component.ts
@@ -1,8 +1,24 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+interface ITransactionFormValue {
+	amount: string;
+	date: Date;
+	category: string;
+	wallet: string;
+	description: string;
+}
+
 @Component({
 	templateUrl: './transaction.component.html',
 	styleUrls: ['./transaction.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TransactionComponent {}
+export class TransactionComponent {
+	formValue: ITransactionFormValue = {
+		amount: undefined,
+		date: new Date(),
+		category: undefined,
+		wallet: undefined,
+		description: undefined,
+	};
+}
