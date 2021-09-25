9ac8f000b8c29f2e91695bd89d677d08a0f0fe96
Fidores <45700014+Fidores@users.noreply.github.com>
2021-09-26 00:08:34 +0200

    Integrated the component with the data from the server

--- src/app/pages/transaction/transaction.component.ts
+++ src/app/pages/transaction/transaction.component.ts
@@ -1,5 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ICategory } from 'src/app/common/interfaces/category';
+import { IWallet } from 'src/app/common/interfaces/wallet';
+import { CategoriesService } from 'src/app/services/categories/categories.service';
+import { LoadingService } from 'src/app/services/loading/loading.service';
+import { WalletsService } from 'src/app/services/wallets/wallets.service';
 
 interface ITransactionFormValue {
 	amount: string;
@@ -14,11 +21,15 @@ interface ITransactionFormValue {
 	styleUrls: ['./transaction.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TransactionComponent {
-	constructor(private readonly _route: ActivatedRoute) {}
+export class TransactionComponent implements OnInit {
+	constructor(
+		private readonly _route: ActivatedRoute,
+		private readonly _categories: CategoriesService,
+		private readonly _wallets: WalletsService,
+		private readonly _loading: LoadingService
+	) {}
 
 	isInEditState = !!this._route.snapshot.paramMap.get('id');
-
 	formValue: ITransactionFormValue = {
 		amount: undefined,
 		date: new Date(),
@@ -26,6 +37,19 @@ export class TransactionComponent {
 		wallet: undefined,
 		description: undefined,
 	};
+
+	data$: Observable<{
+		categories: ICategory[];
+		wallets: IWallet[];
+	}>;
+
+	ngOnInit() {
+		this.data$ = this._loading.add(
+			combineLatest([this._categories.readAll(), this._wallets.getAll()]).pipe(
+				map(([categories, wallets]) => ({ categories, wallets }))
+			)
+		);
+	}
 
 	create() {
 		console.log('Creating a transaction');
