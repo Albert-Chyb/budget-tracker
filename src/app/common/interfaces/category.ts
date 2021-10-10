193e78a71736dab14fb0bc7c2ebc6b802c562fde
Albert Chyb <albert7773000@gmail.com>
2021-10-10 22:19:18 +0200

    Organized the types

--- src/app/common/interfaces/category.ts
+++ src/app/common/interfaces/category.ts
@@ -16,3 +16,9 @@ export interface ICategoryBase {
 	icon: string;
 	iconPath: string;
 }
+
+export interface ICategoryCreatePayload extends ICategoryBase {}
+
+export interface ICategoryUpdatePayload extends Partial<ICategoryBase> {}
+
+export interface ICategoryReadPayload extends ICategoryBase {}
--- src/app/services/categories/categories.service.ts
+++ src/app/services/categories/categories.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireFunctions } from '@angular/fire/functions';
+import { Observable } from 'rxjs';
 import { first, switchMap } from 'rxjs/operators';
 import { FirebaseCallableFunctionsNames } from 'src/app/common/firebase-callable-functions';
-import { ICategory, ICategoryBase } from 'src/app/common/interfaces/category';
+import {
+	ICategory,
+	ICategoryCreatePayload,
+	ICategoryReadPayload,
+	ICategoryUpdatePayload,
+} from 'src/app/common/interfaces/category';
 import { UserService } from '../user/user.service';
 
 @Injectable({
@@ -16,11 +22,11 @@ export class CategoriesService {
 		private readonly _afFunctions: AngularFireFunctions
 	) {}
 
-	async create(category: ICategoryBase, id?: string): Promise<void> {
+	async create(category: ICategoryCreatePayload, id?: string): Promise<void> {
 		const uid = await this._user.getUid();
 		const docID = id ?? this._afStore.createId();
 		const docRef = await this._afStore
-			.doc(`users/${uid}/categories/${docID}`)
+			.doc<ICategoryReadPayload>(`users/${uid}/categories/${docID}`)
 			.ref.get();
 
 		if (docRef.exists) {
@@ -32,41 +38,42 @@ export class CategoriesService {
 		return docRef.ref.set(category);
 	}
 
-	read(id: string) {
+	read(id: string): Observable<ICategory> {
 		return this._user
 			.getUid$()
 			.pipe(
 				switchMap(uid =>
 					this._afStore
-						.doc<ICategory>(`users/${uid}/categories/${id}`)
+						.doc<ICategoryReadPayload>(`users/${uid}/categories/${id}`)
 						.valueChanges({ idField: 'id' })
 				)
 			);
 	}
 
-	readAll() {
+	readAll(): Observable<ICategory[]> {
 		return this._user
 			.getUid$()
 			.pipe(
 				switchMap(uid =>
 					this._afStore
-						.collection<ICategory>(`users/${uid}/categories`, query =>
-							query.orderBy('name', 'asc')
+						.collection<ICategoryReadPayload>(
+							`users/${uid}/categories`,
+							query => query.orderBy('name', 'asc')
 						)
 						.valueChanges({ idField: 'id' })
 				)
 			);
 	}
 
-	async update(id: string, category: Partial<ICategoryBase>) {
+	async update(id: string, category: ICategoryUpdatePayload): Promise<void> {
 		const uid = await this._user.getUid();
 
 		return this._afStore
-			.doc<ICategory>(`users/${uid}/categories/${id}`)
+			.doc<ICategoryReadPayload>(`users/${uid}/categories/${id}`)
 			.update(category);
 	}
 
-	async delete(id: string): Promise<any> {
+	async delete(id: string): Promise<void> {
 		const deleteCategory = this._afFunctions.httpsCallable(
 			FirebaseCallableFunctionsNames.DeleteCategory
 		);
