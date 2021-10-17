3ea5f402b9799ed8eb6e86905b13056c7ebc5dd7
Albert Chyb <albert7773000@gmail.com>
2021-10-17 21:00:53 +0200

    Added types

--- src/app/common/interfaces/firestore.ts
+++ src/app/common/interfaces/firestore.ts
@@ -2,3 +2,8 @@ import firebase from 'firebase/app';
 
 export type Timestamp = firebase.firestore.Timestamp;
 export type FieldValue = firebase.firestore.FieldValue;
+export type DocumentData = firebase.firestore.DocumentData;
+export type FirestoreDataConverter<T> =
+	firebase.firestore.FirestoreDataConverter<T>;
+export type QueryDocumentSnapshot<T> =
+	firebase.firestore.QueryDocumentSnapshot<T>;
