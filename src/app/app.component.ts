ec152adfecf2b3f1eefd8d9fa53ab5524798e2a2
Albert Chyb <albert7773000@gmail.com>
2021-11-02 17:59:01 +0100

    Moved side-nav logic to a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
-import { MatDrawerMode } from '@angular/material/sidenav';
-import { Observable } from 'rxjs';
-import { distinctUntilChanged, map } from 'rxjs/operators';
-import { Breakpoint } from './common/breakpoints';
 
 @Component({
 	selector: 'app-root',
@@ -13,38 +8,4 @@ import { Breakpoint } from './common/breakpoints';
 		class: 'with-fixed-mat-toolbar',
 	},
 })
-export class AppComponent {
-	constructor(private readonly _breakpointObserver: BreakpointObserver) {}
-
-	private readonly _correspondingMode: Map<Breakpoint, MatDrawerMode> = new Map(
-		[
-			[Breakpoint.XSmall, 'over'],
-			[Breakpoint.Small, 'over'],
-			[Breakpoint.Medium, 'side'],
-			[Breakpoint.Large, 'side'],
-			[Breakpoint.XLarge, 'side'],
-			[Breakpoint.XXLarge, 'side'],
-		]
-	);
-
-	sidenavMode$: Observable<MatDrawerMode> = this._breakpointObserver
-		.observe([
-			Breakpoint.XSmall,
-			Breakpoint.Small,
-			Breakpoint.Medium,
-			Breakpoint.Large,
-			Breakpoint.XLarge,
-			Breakpoint.XXLarge,
-		])
-		.pipe(
-			map(result => {
-				const breakpoints = Object.entries(result.breakpoints);
-
-				return breakpoints.find(([, value]) => value)[0];
-			}),
-			map(breakpoint => {
-				return this._correspondingMode.get(breakpoint as any) ?? null;
-			}),
-			distinctUntilChanged()
-		);
-}
+export class AppComponent {}
