902b081085a80720f2052433f346094850cca1e2
Albert Chyb <albert7773000@gmail.com>
2022-03-01 22:13:55 +0100

    Exposed the viewport height as a CSS variable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+	Component,
+	Inject,
+	OnInit,
+	Renderer2,
+	RendererStyleFlags2,
+} from '@angular/core';
 import { MainSidenavService } from './services/main-sidenav/main-sidenav.service';
 
 @Component({
@@ -9,6 +16,34 @@ import { MainSidenavService } from './services/main-sidenav/main-sidenav.service
 		class: 'with-fixed-mat-toolbar',
 	},
 })
-export class AppComponent {
-	constructor(public mainSidenav: MainSidenavService) {}
+export class AppComponent implements OnInit {
+	constructor(
+		public mainSidenav: MainSidenavService,
+		private readonly _renderer: Renderer2,
+		@Inject(DOCUMENT) private readonly _doc: Document
+	) {}
+
+	ngOnInit() {
+		/*
+			It is for cases, where 100vh wouldn't work.
+			For example on Safari, 100vh equals the viewport when the url bar is hidden.
+			However the appearing of the bar does not decrease the 100vh value.
+
+			Javascript is not affected by this behavior, 
+			and therefore exposing the viewport height by a CSS variable is the best option so far.
+		*/
+		this._updateViewportHeight();
+		this._renderer.listen(window, 'resize', () => this._updateViewportHeight());
+	}
+
+	private _updateViewportHeight() {
+		const viewportHeight = window.innerHeight;
+
+		this._renderer.setStyle(
+			this._doc.body,
+			'--viewport-height',
+			`${viewportHeight}px`,
+			RendererStyleFlags2.DashCase
+		);
+	}
 }
