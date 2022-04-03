6e5ec5d4c0da3cd5d8f11fcbd74e08f91f2e1a50
Albert Chyb <albert7773000@gmail.com>
2022-04-03 21:22:33 +0200

    Added checking if a transaction exists

--- src/app/pages/edit-transaction/edit-transaction.component.ts
+++ src/app/pages/edit-transaction/edit-transaction.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
-import { combineLatest } from 'rxjs';
-import { first, map, switchMap, takeWhile } from 'rxjs/operators';
+import { combineLatest, of, throwError } from 'rxjs';
+import { catchError, first, map, switchMap, takeWhile } from 'rxjs/operators';
 import { isNullish } from 'src/app/common/helpers/isNullish';
 import { ITransactionUpdatePayload } from 'src/app/common/interfaces/transaction';
 import { TransactionFormValue } from 'src/app/components/transaction-form/transaction-form.component';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
+import { ErrorsService } from 'src/app/services/errors/errors.service';
 import { LoadingService } from 'src/app/services/loading/loading.service';
 import { TransactionsService } from 'src/app/services/transactions/transactions.service';
 import { WalletsService } from 'src/app/services/wallets/wallets.service';
@@ -24,14 +25,31 @@ export class EditTransactionComponent {
 		private readonly _categories: CategoriesService,
 		private readonly _router: Router,
 		private readonly _snackBar: MatSnackBar,
-		private readonly _loading: LoadingService
+		private readonly _loading: LoadingService,
+		private readonly _errors: ErrorsService
 	) {}
 
 	readonly data$ = combineLatest([
 		this._wallets.list(),
 		this._categories.list(),
 		this._route.paramMap.pipe(
-			switchMap(params => this._transactions.read(params.get('id'))),
+			map(params => params.get('id')),
+			switchMap(transactionId =>
+				this._transactions
+					.exists(transactionId)
+					.pipe(
+						switchMap(exists =>
+							exists
+								? this._transactions.read(transactionId)
+								: throwError(new Error('Transaction does not exist.'))
+						)
+					)
+			),
+			catchError(error => {
+				this._errors.show('Nie odnaleziono transakcji');
+				this._router.navigateByUrl('/transactions');
+				return of(null);
+			}),
 			takeWhile(transaction => !isNullish(transaction))
 		),
 	]).pipe(
@@ -58,10 +76,10 @@ export class EditTransactionComponent {
 			)
 			.subscribe(
 				() => {
-					this._snackBar.open('Pomyślnie zaktualizowano transakcje');
+					this._snackBar.open('Pomyślnie zaktualizowano transakcje.');
 					this._router.navigateByUrl('/');
 				},
-				() => this._snackBar.open('Nie udało się zaktualizować transakcji')
+				() => this._errors.show('Nie udało się zaktualizować transakcji.')
 			);
 	}
 
@@ -73,7 +91,15 @@ export class EditTransactionComponent {
 					first()
 				)
 			)
-			.subscribe(() => this._router.navigateByUrl('/'));
+			.subscribe(
+				() => {
+					this._router.navigateByUrl('/');
+					this._snackBar.open('Usunięto transakcje.');
+				},
+				() => {
+					this._errors.show('Nie udało się usunąć transakcji');
+				}
+			);
 	}
 
 	handleFormSubmit(formValue: TransactionFormValue) {
