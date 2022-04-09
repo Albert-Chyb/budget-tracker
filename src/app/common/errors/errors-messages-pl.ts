875b35cfc935104c4a170f027623253adeafaf03
Albert Chyb <albert7773000@gmail.com>
2022-04-09 23:04:41 +0200

    Created a map that contains error messages

---
+++ src/app/common/errors/errors-messages-pl.ts
@@ -0,0 +1,40 @@
+import { ErrorCode } from './error-code';
+
+/**
+ * Contains messages associated with error codes.
+ */
+const errorMessages = new Map<string, string>([
+	['default', 'Wystąpił nieoczekiwany błąd.'],
+	[ErrorCode.TransactionNotFound, 'Transakcja nie została znaleziona.'],
+
+	// Firebase auth errors.
+	['auth/credential-already-in-use', 'Wybrane konto istnieje już w serwisie.'],
+	[
+		'auth/cancelled-popup-request',
+		'Tylko jedno okno logowania może być otwarte w tym samym czasie.',
+	],
+	[
+		'auth/operation-not-supported-in-this-environment',
+		'Logowanie może odbyć się tylko poprzez protoków http lub https.',
+	],
+	[
+		'auth/popup-blocked',
+		'Okno logowania zostało zablokowane przez przeglądarkę. Sprawdź ustawienia okien Popup dla tej strony.',
+	],
+	[
+		'auth/popup-closed-by-user',
+		'Okno logowania zostało zamknięte za wcześnie.',
+	],
+]);
+
+export const DEFAULT_ERROR_CODE = 'default';
+
+export const getErrorMessage = (code: string): string => {
+	code = errorMessages.has(code) ? code : DEFAULT_ERROR_CODE;
+
+	if (code === DEFAULT_ERROR_CODE && !errorMessages.has(DEFAULT_ERROR_CODE)) {
+		throw new Error('The default error message was not set.');
+	}
+
+	return errorMessages.get(code);
+};
--- src/app/common/errors/global-error-handler.ts
+++ src/app/common/errors/global-error-handler.ts
@@ -1,8 +1,6 @@
 import { ErrorHandler, Injectable, NgZone } from '@angular/core';
-import { FirebaseError } from '@angular/fire/app';
 import { ErrorsService } from '@services/errors/errors.service';
-import { AppError } from './app-error';
-import { ErrorCode } from './error-code';
+import { DEFAULT_ERROR_CODE, getErrorMessage } from './errors-messages-pl';
 
 @Injectable({
 	providedIn: 'root',
@@ -14,70 +12,14 @@ export class GlobalErrorHandler implements ErrorHandler {
 	) {}
 
 	handleError(unhandled: any) {
-		// Angular wraps unhandled errors in an Error object.
-		// Actual thrown error is placed in the 'rejection' property.
 		const actualThrown = this._extractThrownError(unhandled);
 
-		if (
-			actualThrown instanceof FirebaseError &&
-			actualThrown.code.startsWith('auth')
-		) {
-			this._handleFirebaseAuthError(<any>actualThrown);
-		} else if (actualThrown instanceof AppError) {
-			this._handleAppError(actualThrown);
+		if ('code' in actualThrown) {
+			this._showMessage(getErrorMessage(actualThrown.code));
 		} else {
-			// Throw other errors back to the console.
+			this._showMessage(getErrorMessage(DEFAULT_ERROR_CODE));
 			console.error(actualThrown);
 		}
-	}
-
-	private _handleAppError(error: AppError) {
-		let msg: string;
-
-		switch (error.code) {
-			case ErrorCode.TransactionNotFound:
-				msg = 'Transakcja nie została znaleziona.';
-				break;
-
-			default:
-				msg = 'Wystąpił nieoczekiwany błąd.';
-				break;
-		}
-
-		this._errors.show(msg);
-	}
-
-	private _handleFirebaseAuthError(error: FirebaseError) {
-		let msg: string = '';
-
-		switch (error.code) {
-			case 'auth/credential-already-in-use':
-				msg = 'Wybrane konto istnieje już w serwisie.';
-				break;
-
-			case 'auth/cancelled-popup-request':
-				msg = 'Tylko jedno okno logowania może być otwarte w tym samym czasie.';
-				break;
-
-			case 'auth/operation-not-supported-in-this-environment':
-				msg = 'Logowanie może odbyć się tylko poprzez protoków http lub https.';
-				break;
-
-			case 'auth/popup-blocked':
-				msg =
-					'Okno logowania zostało zablokowane przez przeglądarkę. Sprawdź ustawienia okien Popup dla tej strony.';
-				break;
-
-			case 'auth/popup-closed-by-user':
-				msg = 'Okno logowania zostało zamknięte za wcześnie.';
-				break;
-
-			default:
-				msg = 'Podczas autoryzacji wystąpił nieoczekiwany błąd.';
-				break;
-		}
-
-		if (msg) this._showMessage(msg);
 	}
 
 	private _showMessage(message: string) {
@@ -85,6 +27,9 @@ export class GlobalErrorHandler implements ErrorHandler {
 	}
 
 	private _extractThrownError(error: any) {
+		// Angular wraps unhandled errors in an Error object.
+		// Actual thrown error is placed in the 'rejection' property.
+
 		return 'rejection' in error ? error.rejection : error;
 	}
 }
