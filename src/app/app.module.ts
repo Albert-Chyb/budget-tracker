f96bfeb1a8b1196b46bb1965bc43ae5f42fae4ae
Albert Chyb <albert7773000@gmail.com>
2022-07-08 21:52:16 +0200

    Configured the app as a PWA app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { AuthGuardModule } from '@angular/fire/auth-guard';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { GlobalErrorHandler } from '@common/errors/global-error-handler';
 import { AppLayoutModule } from '@modules/app-layout.module';
 import { CategoriesModule } from '@modules/categories.module';
@@ -12,6 +13,7 @@ import { SharedModule } from '@modules/shared.module';
 import { TransactionsModule } from '@modules/transactions.module';
 import { WalletsModule } from '@modules/wallets.module';
 import { LoginComponent } from '@pages/login/login.component';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -28,6 +30,12 @@ import { AppComponent } from './app.component';
 		DashboardModule,
 		AppLayoutModule,
 		FirebaseModule,
+		ServiceWorkerModule.register('ngsw-worker.js', {
+			enabled: environment.production,
+			// Register the ServiceWorker as soon as the application is stable
+			// or after 30 seconds (whichever comes first).
+			registrationStrategy: 'registerWhenStable:30000',
+		}),
 	],
 	providers: [
 		{
