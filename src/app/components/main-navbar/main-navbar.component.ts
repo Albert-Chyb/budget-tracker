dda640687d754ba04de39ce0a7d03d0dd4b5a7c7
Albert Chyb <albert7773000@gmail.com>
2022-07-11 21:05:22 +0200

    Removed flickering effect of the loading indicator

--- src/app/components/main-navbar/main-navbar.component.ts
+++ src/app/components/main-navbar/main-navbar.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AuthService } from '@services/auth/auth.service';
 import { LoadingService } from '@services/loading/loading.service';
 import { MainSidenavService } from '@services/main-sidenav/main-sidenav.service';
-import { combineLatest, Observable, of } from 'rxjs';
-import { debounceTime, map } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
+/**
+ * Number of milliseconds to wait until the loading indicator is shown.
+ * This will prevent the indicator to show up during quick actions,
+ * that may not need indicating the loading state.
+ */
+const LOADER_DELAY: number = 150;
 
 @Component({
 	selector: 'main-navbar',
@@ -11,34 +17,18 @@ import { debounceTime, map } from 'rxjs/operators';
 	styleUrls: ['./main-navbar.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainNavbarComponent implements OnInit {
+export class MainNavbarComponent {
 	constructor(
 		private readonly _auth: AuthService,
 		private readonly _loading: LoadingService,
 		private readonly _sidenav: MainSidenavService
 	) {}
 
-	isLoggedIn$ = this._auth.isLoggedIn$;
-	shouldDisplayLoader$ = this._loading.isLoading$.pipe(debounceTime(50));
-
-	data$: Observable<{
-		isLoggedIn: boolean;
-		shouldDisplayLoader: boolean;
-	}>;
-
-	ngOnInit() {
-		this.data$ = combineLatest([
-			this._auth.isLoggedIn$,
-			this._loading.isLoading$,
-			of(''),
-		]).pipe(
-			map(([isLoggedIn, isLoading, routeNameChange]) => ({
-				isLoggedIn,
-				shouldDisplayLoader: isLoading,
-				routeNameChange,
-			}))
-		);
-	}
+	readonly isLoggedIn$ = this._auth.isLoggedIn$;
+	readonly shouldDisplayLoader$ = this._loading.isLoading$.pipe(
+		debounceTime(LOADER_DELAY),
+		distinctUntilChanged()
+	);
 
 	logout() {
 		this._auth.logout();
